e028c0ea3152f91d391286a8c59d279699e977d0
lk <lj.k@outlook.com>
2021-01-02 16:50:18 +0800

    fix repo

---
+++ scripts/init_gitremote.sh
@@ -0,0 +1,3 @@
+git remote add gitee https://gitee.com/inspur-risc-v/RVWeekly
+git remote add inspur http://open.inspur.com/riscv/RVWeekly
+git remote add github https://github.com/inspur-risc-v/RVWeekly
